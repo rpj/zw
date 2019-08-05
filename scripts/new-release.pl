3a57a73f0192647a65e392ecc71e78461ad5dd25
Ryan Joseph <ryan.joseph@gmail.com>
2019-08-05 06:52:43 -0700

    Add 'info mode' to new-release.pl

--- scripts/new-release.pl
+++ scripts/new-release.pl
@@ -1,29 +1,40 @@
 #!/usr/bin/perl
 
-my $buildDir = shift || die "build dir\n\n";
-my $destDir = shift || die "dest dir\n\n";
+# symlink the script as "release-info.pl" to enable this mode
+$infoMode = 1, if ($0 =~ /release-info/i);
+
 my $version = shift || die "version\n\n";
+my $destDir = shift || die "dest dir\n\n";
+my $buildDir = shift || (!$infoMode && die "build dir\n\n");
 
 $version =~ s/v//ig;
-$buildDir .= "/zero_watch.ino.bin";
 
-die "$buildDir does not exist\n\n", unless (-e $buildDir && !(-d $buildDir));
+my $tFile = "zero_watch-v$version.ino.bin";
+my $target = "$destDir/$tFile";
+
+if (!$infoMode) {
+    $buildDir .= "/zero_watch.ino.bin";
+    die "$buildDir does not exist\n\n", unless (-e $buildDir && !(-d $buildDir));
+}
+else {
+    $buildDir = $target;
+}
+
 die "$destDir does not exist\n\n", unless (-e $destDir && -d $destDir);
 
 die "Bad binary version\n\n", unless (`strings $buildDir` =~ /v([0-9\.]+)/);
 my $binVer = $1;
 die "Versions don't match ($binVer vs $version)\n\n", unless ($version eq $binVer);
 
-my $tFile = "zero_watch-v$version.ino.bin";
-my $target = "$destDir/$tFile";
-
 my $md5 = (split(/\s/, `md5sum $buildDir`))[0];
 `md5sum $buildDir > $target.md5sum.txt`;
 
 my $bSize = (stat($buildDir))[7];
 
-`cp $buildDir $target`;
-die "Copy failed ($?)\n\n", if ($?);
+if (!$infoMode) {
+    `cp $buildDir $target`;
+    die "Copy failed ($?)\n\n", if ($?);
+}
 
 print <<__EOF__;
 {
